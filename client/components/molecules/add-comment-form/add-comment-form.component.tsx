b8306c11fc2cc62e2c2f5bd613f42c95d1bfe0c9
Sanmoh Hombal <sanmoh.hombal@gmail.com>
2022-08-17 22:02:14 +0530

    🔨  AthAddCommentForm Migration
    Uses AthButton and AthInput Components and extends native form attributes

---
+++ client/components/molecules/add-comment-form/add-comment-form.component.tsx
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+
+import AthButton from "@client/components/atoms/button";
+import AthInput from "@client/components/atoms/input";
+
+export interface IAthAddCommentFormProps extends React.FormHTMLAttributes<HTMLFormElement> {
+	loading?: boolean;
+}
+
+const AthAddCommentForm: React.FC<IAthAddCommentFormProps> = ({
+	children,
+	loading,
+	...rest
+}: IAthAddCommentFormProps): JSX.Element => {
+	const [comment, setComment] = useState<string>("");
+
+	return (
+		<form {...rest} className={`flex ${rest.className}`}>
+			<AthInput
+				placeholder="What are your thoughts?"
+				loading={loading}
+				value={comment}
+				onInput={(event) => setComment((event.target as HTMLInputElement).value)}
+				className="flex-1"
+			/>
+			<AthButton loading={loading} disabled={!comment} type="submit">
+				Submit
+			</AthButton>
+		</form>
+	);
+};
+
+export default AthAddCommentForm;

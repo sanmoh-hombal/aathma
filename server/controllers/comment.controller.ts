14db6b8af4af9b19faa10563f77cd8199e8762fb
Sanmoh Hombal <sanmoh.hombal@gmail.com>
2022-08-19 14:14:37 +0530

    ✨  Nesting API & Pagination
    The Comment Get and Post APIs now accept parentIds so that nesting can be made possible + pagination
    accepting so that we can achieeve better UX on listing comments both on root and nested level

--- server/controllers/comment.controller.ts
+++ server/controllers/comment.controller.ts
@@ -4,9 +4,17 @@ import { PrismaClientValidationError } from "@prisma/client/runtime";
 
 import { CommentService } from "@server/services";
 
-export async function get(_request: Request, response: Response, _next: NextFunction): Promise<void> {
+export async function get(request: Request, response: Response, _next: NextFunction): Promise<void> {
 	try {
-		response.status(200).json(await CommentService.get());
+		response
+			.status(200)
+			.json(
+				await CommentService.get(
+					parseInt(request.query.page?.toString() || CommentService.DEFAULT_PAGE.toString()),
+					parseInt(request.query.pageSize?.toString() || CommentService.PAGE_SIZE.toString()),
+					request.body.parentId,
+				),
+			);
 	} catch (e: Error | any) {
 		response.status(500).send(e.message);
 	}
@@ -14,7 +22,9 @@ export async function get(_request: Request, response: Response, _next: NextFunc
 
 export async function post(request: Request, response: Response, _next: NextFunction): Promise<void> {
 	try {
-		response.status(200).json(await CommentService.add(request.body.content, request.body.userId || null));
+		response
+			.status(200)
+			.json(await CommentService.add(request.body.content, request.body.userId || null, request.body.parentId || null));
 	} catch (e: Error | PrismaClientValidationError | any) {
 		if (e instanceof PrismaClientValidationError) response.status(500).send(e.message);
 		else response.status(500).send(e.message);
--- server/services/comment.service.ts
+++ server/services/comment.service.ts
@@ -1,20 +1,35 @@
 import { randomUUID } from "crypto";
 
 import { PrismaClient } from "@global/clients";
-import { IComment, ICommentUserUpvote } from "@global/types/comment.type";
+import { ICommentUserUpvote } from "@global/types/comment.type";
 import { IUser } from "@global/types/user.type";
 
 import { UserService } from "@server/services";
 
+export const DEFAULT_PAGE: number = 1;
+export const PAGE_SIZE: number = 10;
+
 /**
- * It returns an array of comment objects descending based on created
- * @return {Array<IComment>} An array of comments
+ * It returns a list of comments, ordered by creation date, with a maximum of `pageSize` comments per
+ * page, starting from the `page`th page
+ * @param {number} page - The page number of the comments to return.
+ * @param {number} pageSize - number = PAGE_SIZE (10),
+ * @param {string} [parentId] - The id of the parent comment. If this is not provided, it will return
+ * the top-level comments.
+ * @return {Array<ICommentUserUpvote>} An array of comments
  */
-export async function get(): Promise<Array<IComment>> {
+export async function get(
+	page: number = DEFAULT_PAGE,
+	pageSize: number = PAGE_SIZE,
+	parentId?: string,
+): Promise<Array<ICommentUserUpvote>> {
 	try {
 		return await PrismaClient.comment.findMany({
+			where: { parentId: parentId || null },
 			orderBy: { created: "desc" },
-			include: { user: true, upvotes: true },
+			take: pageSize,
+			skip: (page - 1) * pageSize,
+			include: { user: true, upvotes: true, children: { include: { user: true, upvotes: true } } },
 		});
 	} catch (e) {
 		throw e;
@@ -27,9 +42,10 @@ export async function get(): Promise<Array<IComment>> {
  * It adds a comment to the database, and if the userId is not provided, it creates a new user
  * @param {string} content - string - The content of the comment
  * @param {string} [userId] - The id of the user who created the comment. If not provided, a new user will be created.
+ * @param {string} [parentId] - The id of the parent comment so that nesting can be possible.
  * @return {ICommentUserUpvote} A promise that resolves to a ICommentUser object with user and upvotes
  */
-export async function add(content: string, userId?: string): Promise<ICommentUserUpvote> {
+export async function add(content: string, userId?: string, parentId?: string): Promise<ICommentUserUpvote> {
 	try {
 		let user: IUser | null = null;
 
@@ -37,8 +53,8 @@ export async function add(content: string, userId?: string): Promise<ICommentUse
 		if (!user || !userId) user = await UserService.add();
 
 		const comment: ICommentUserUpvote = await PrismaClient.comment.create({
-			data: { id: randomUUID(), userId: user.id, content },
-			include: { user: true, upvotes: true },
+			data: { id: randomUUID(), userId: user.id, parentId, content },
+			include: { user: true, upvotes: true, parent: true },
 		});
 
 		return comment;
